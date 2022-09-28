56de55d222254358d9d88adc7de8a4b3132e4e44
ack61 <85378035+ack61@users.noreply.github.com>
2022-09-28 20:24:05 +0900

    周期を計算できるようにした

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const button = document.querySelector('input[type=button]');
+const period_button = document.querySelector('#period_button');
 const input = document.querySelector('input[type=text]');
 const input_length = document.querySelector('#getLength');
 const table = document.querySelector('table');
 const paragraph = document.querySelector('p');
 
 button.addEventListener('click', updateButton);
+period_button.addEventListener('click', preCalcPeriod);
 
 function updateButton() {
   console.log(input.value);
@@ -37,6 +39,77 @@ function updateButton() {
     S[i] = min;
 //    console.log(min);
     add(i, min);
+  }
+}
+
+function preCalcPeriod() {
+  setTimeout(calcPeriod, 1);
+}
+
+function calcPeriod() {
+  console.log(input.value);
+  while(table.firstChild) {
+      table.removeChild(table.firstChild);
+  }
+  let G = input.value.split(',').map(Number);
+  let S = [];
+  let old = 128;
+  let n = 256;
+  S[0] = 0;
+  let index = 1;
+  let finish = false;
+
+  while(true){
+    let exist = [];
+    for(let j = 0; j < G.length; j++){
+      let next = Math.max(0, index - G[j]);
+//      console.log(next);
+      exist.push(S[next]);
+    }
+    exist.sort((a, b) => a - b);
+//    console.log(exist);
+    let min = 0;
+    for(let j = 0; j < G.length; j++){
+//      console.log("min:" + min + ", ex:"+exist[j]);
+      if(min == exist[j]){
+        min++;
+      }else if(min != exist[j] + 1){
+        break;
+      }
+    }
+    S[index] = min;
+//    console.log(min);
+//    add(index, min);
+
+    if(index == n){
+      for(let i = old + 1; i < n - G[G.length-1]; i++){
+        let ok = true;
+        for(let j = 0; j < G[G.length-1]; j++){
+          if(S[old+j] != S[i+j]){
+            ok = false;
+            break;
+          }
+        }
+        if(ok){
+          console.log("@" + (i - old));          
+          finish = true;
+          tr = document.createElement('tr');
+          tr.textContent = i - old;
+          table.appendChild(tr);
+          break;
+        }
+      }
+      old = n;
+      n *= 2;
+    }
+
+    if(index > 1000000){
+      break;
+    }
+    if(finish){
+      break;
+    }
+    index++;
   }
 }
 
